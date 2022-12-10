8625e80d6d7623c8bcadc83028240b6bb2609858
Dima <Uchiha132132qqq@gmail.com>
2022-12-10 03:28:49 +0300

    try to add gh pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,17 +5,27 @@ import { Login } from "../Login/Login";
 import { TodoList } from "../TodoList/TodoList";
 import { Header } from "../Header/Header";
 
-export const App:FC = () => {
+export const App: FC = () => {
   return (
     <main>
       <Header />
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/todos" element={<TodoList item={{
-          title: undefined,
-          completed: false,
-          id: undefined
-        }} id={undefined} completed={false} title={undefined} />}></Route>
+        <Route
+          path="/todos"
+          element={
+            <TodoList
+              item={{
+                title: undefined,
+                completed: false,
+                id: undefined,
+              }}
+              id={undefined}
+              completed={false}
+              title={undefined}
+            />
+          }
+        ></Route>
       </Routes>
     </main>
   );
